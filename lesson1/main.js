e7c096e2d5061e95cbb068ea23d13907ff314768
Admin <admin@vinasupport.com>
2023-09-01 22:00:21 +0700

    second commit

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,1 +1,76 @@
-alert('Hi, Java Script basic!')
+// cách khai báo biến
+/**comment 1 khoi */
+// vô hiệu hóa code
+// var fullName = "Tran Duc Hung";
+// var age = "20";
+// alert(fullName);
+// alert(age);
+/**
+ * Giới thiệu 1 số hàm built-in
+ * 1. Alert: mesange
+ * 2. Console: 
+ * 3. Confirm:
+ 
+ * 4. Prompt: kêt họp alert + confirm + hop thoai input
+ * 5. Set timeout
+ * 6. Set interval
+ * * 1 2 3: bật cửa sổ hộp thoại
+ * console.log();
+confirm('Xac nhan ban du tuoi!');
+prompt('Xac nhan doi tuoi');
+setTimeout(function() {
+    alert('thong bao');
+}, 1000)
+setInterval(function() {
+    console.log('day la thong bao' + Math.random());
+}, 1000)
+ */
+
+
+
+
+
+/* Lam quen voi toan tu
+ Gioi thieu toan tu trong JavaScript
+1. Toan tu so hoc - Agrithmetic
+    var a = 1 * 2;
+    console.log(a);
+2. Toan tu gan - Assignment
+    var fullName = "Tran Duc Hung";
+3. Toan tu so sanh - Comparison
+    var a = 1;
+    var b = 2;
+    if (a>b){
+        alert('dung');
+    }
+4. Toan tu logic - logical
+    var a = 1
+    var b = 2
+    if(a > 0 || b>0){
+        alert('a b lon hon 0')
+    }
+*/
+
+
+
+
+/** Toan tu so hoc
+ *  +  cộng
+ *  -  trừ
+ *  *  Nhân
+ *  **  Lũy thừa
+ *  /   chia
+ *  %   chia lấy dư
+ *  ++  tăng 1 giá trị số 
+ *  --  giảm 1 giá trị số
+ */
+
+/** Toán tử gán
+    =
+    +=
+    -=
+    *=
+    /=      x /= y  x = x/y
+    **=   x **= y --> x = x ** y
+ */
+
